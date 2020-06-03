3fd7b539c2e722a467de41e187e4a8896fc5280a
Cody <wildtyto@gmail.com>
2020-06-03 11:57:26 +0800

    Prepared basic

---
+++ src/reducers.js
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux';
+
+const orders = (state = [], action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default combineReducers({
+  orders
+});
